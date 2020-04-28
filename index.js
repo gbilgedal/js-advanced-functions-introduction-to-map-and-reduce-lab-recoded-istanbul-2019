db73f6d950241db320878d4447d773ee61e9cca0
G Bilge Dal <lagala999@gmail.com>
2020-04-28 11:53:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function mapToNegativize(sourceArray){
+  newArray = [];
+  for(let i = 0;i<sourceArray.length;i++){
+    newArray.push(sourceArray[i]*-1);
+  }
+  return newArray;
+}
+function mapToNoChange(sourceArray){
+  newArray = [];
+  for(let i = 0;i<sourceArray.length;i++){
+    newArray.push(sourceArray[i]);
+  }
+  return newArray;
+}
+function mapToDouble(sourceArray){
+  newArray = [];
+  for(let i = 0;i<sourceArray.length;i++){
+    newArray.push(sourceArray[i]*2);
+  }
+  return newArray;
+}
+function mapToSquare(sourceArray){
+  newArray = [];
+  for(let i = 0;i<sourceArray.length;i++){
+    newArray.push(sourceArray[i]*sourceArray[i]);
+  }
+  return newArray;
+}
